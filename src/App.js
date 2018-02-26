3b698f3c3b897e25f660cfb326589fbf7670acaf
Rabvc4 <chill_zero@hotmail.com>
2018-02-26 17:06:48 -0600

    Added Game (Battle Grid)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import Game from './Game';
 import InputBox from './InputBox';
 import './App.css';
 
@@ -14,7 +15,10 @@ class App extends Component {
           <h1 className="App-title">Dungeons & Dragons Dice Roller</h1>
         </header>
         <p className="App-intro">
+
+          <Game></Game>
           <InputBox></InputBox>
+
         </p>
       </div>
     );
