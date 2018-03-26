3d165c27c9d6d57b9a39d625d77a0ca99cd80aad
Rabvc4 <chill_zero@hotmail.com>
2018-03-26 14:17:58 -0500

    Made element tags self-closing

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar></NavBar>
+        <NavBar />
         <header className="App-header">
           <img src="https://cdn1.iconfinder.com/data/icons/sin-city-memories/128/dice-red-256.png" className="App-logo" alt="logo" />
           <h1 className="App-title">Dungeons & Dragons Dice Roller</h1>
         </header>
         <div className="App-intro">
 
-          <Game></Game>
-          <InputBox></InputBox>
-          <Chatroom></Chatroom>
+          <Game />
+          <InputBox />
+          <Chatroom />
 
         </div>
       </div>
